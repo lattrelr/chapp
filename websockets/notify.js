c1fdba2f4b688ce121003b4ddf2828c22b581cc4
Ryan Lattrel <ryan.lattrel@gmail.com>
2024-09-22 08:28:50 +0100

    Add DM

--- websockets/notify.js
+++ websockets/notify.js
@@ -2,6 +2,7 @@ const socket = {};
 const WebSocketServer = require('ws').Server
 const authjwt = require ('../middleware/authjwt');
 let ws = null
+const clientMap = new Map();
 
 socket.start = (server) => {
     server.on('upgrade', onUpgrade);
@@ -27,6 +28,8 @@ async function onUpgrade(request, socket, head) {
 
 function handleNew(socket) {
     console.log(`New connection for user ${socket.userId}`)
+    setStatus(socket.userId, "ONLINE");
+    clientMap.set(socket.userId, socket)
 
     socket.on('message', msg => {
         handleMessage(socket, msg)
@@ -38,14 +41,43 @@ function handleNew(socket) {
 }
 
 function handleMessage(socket, msg) {
-    ws.clients.forEach(client => {
-        if (client != socket)
-            client.send(msg)
-    })
+    // TODO validate
+    msg_json = JSON.parse(msg)
+    if (msg_json["to"] != undefined) {
+        let toSocket = clientMap.get(msg_json["to"])
+        if (toSocket != undefined) {
+            directMessage(socket, toSocket, msg)
+        }
+    } else {
+        broadcastMessage(msg)
+    }
+}
+
+function setStatus(user, status) {
+    msg = {
+        "type": "status",
+        "who": user,
+        "status": status
+    }
+    broadcastMessage(JSON.stringify(msg))
 }
 
 function handleClose(socket) {
     console.log(`WebSocket was closed for ${socket.userId}`)
+    setStatus(socket.userId, "OFFLINE");
+    clientMap.delete(socket.userId)
+}
+
+function directMessage(fromSocket, toSocket, msg) {
+    console.log(`From ${fromSocket.userId} to ${toSocket.userId}`)
+    fromSocket.send(msg)
+    toSocket.send(msg)
+}
+
+function broadcastMessage(msg) {
+    ws.clients.forEach(client => {
+        client.send(msg)
+    })
 }
 
 module.exports = socket;
