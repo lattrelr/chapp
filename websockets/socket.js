eefdaaf2d8b97fff63b5c4960cd2dfa972ac082d
Ryan Lattrel <ryan.lattrel@gmail.com>
2024-09-26 02:40:55 +0100

    Try to send a group message.

--- websockets/socket.js
+++ websockets/socket.js
@@ -3,6 +3,7 @@ const WebSocketServer = require('ws').Server
 const authjwt = require ('../middleware/authjwt');
 const statusMessage = require ('./statusMessage');
 const textMessage = require ('./textMessage');
+const db = require("../models");
 let ws = null
 const clientMap = new Map();
 
@@ -48,6 +49,8 @@ function handleNew(socket) {
 }
 
 function handleMessage(socket, msg) {
+    // TODO handle group messages by tagging them as "group" by the client first.
+    // the client will know if it is a group or a user.
     // TODO validate
     // TODO do some sort of ACK and or read receipt
     let toUser = textMessage.getTo(msg)
@@ -56,15 +59,18 @@ function handleMessage(socket, msg) {
         if (toSocket != undefined) {
             directMessage(socket, toSocket, msg)
         } else {
-            console.log(`WebSocket for ${toUser} not found!`)
+            // TODO tag message as group by the client to avoid unwanted lookups.
+            //console.log(`[DM] WebSocket for ${toUser} not found!`)
+            // Try a group message if user DNE
+            // TODO we can use msgkeys someway or another...maybe faster to check that quick.
+            sendGroupMessage(toUser, msg)
         }
-    } else {
-        broadcastMessage(msg)
     }
 }
 
 function setStatus(user, status) {
     msg = statusMessage.get(user, status)
+    // TODO only send to friends or other group members...
     broadcastMessage(msg)
 }
 
@@ -78,6 +84,29 @@ function directMessage(fromSocket, toSocket, msg) {
     console.log(`From ${textMessage.getFrom(msg)} to ${textMessage.getTo(msg)}`)
     fromSocket.send(msg)
     toSocket.send(msg)
+}
+
+function sendGroupMessage(groupId, msg) {
+    // TODO find a way to cache X number of groups so we don't have to keep looking up
+    // who the members are. -- Need to invalidate if members changed!!
+
+    // IF NOT IN CACHE:
+
+    // TODO: FIXME: This isn't working...but the query works manually.
+    db.members.findAll({ where: { "group": groupId } })
+        .then(data => {
+            members.forEach(member => {
+                let toSocket = clientMap.get(member.user)
+                if (toSocket != undefined) {
+                    directMessage(socket, toSocket, msg)
+                } else {
+                    console.log(`[Group] WebSocket for ${toUser} not found!`)
+                }
+            })
+        })
+        .catch(err => {
+            console.log(`Error getting members of ${groupId}`)
+        });
 }
 
 function broadcastMessage(msg) {
